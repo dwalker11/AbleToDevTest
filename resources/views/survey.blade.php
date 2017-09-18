04b54a657dad190819d81f71f575749964206e8a
Devon Walker <devon@winterfell.localdoamin>
2017-09-18 13:19:22 -0400

    Vueified the front end

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -1,61 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
+  <div class="container" id="app1">
     <div class="row">
       <div class="col-md-12">
         <h1>Wellness Survey</h1>
         <p>Please answer the following questions.</p>
       </div>
-    </div>
-
-    <hr>
-
-    <div class="row">
       <div class="col-md-12">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Question #1</h3>
-          </div>
-          <div class="panel-body">
-            <div>What did you eat for breakfast?</div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                Option one is this and that&mdash;be sure to include why it's great
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                Option two can be something else and selecting it will deselect option one
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-                Option three is disabled
-              </label>
-            </div>
-          </div>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="00" aria-valuemin="0" aria-valuemax="100" v-bind:style="{ width: (completeness * 100) + '%' }">@{{ (completeness * 100) + '%' }}</div>
         </div>
-        <button class="btn btn-default" type="button">Prev</button>
-        <button class="btn btn-primary" type="button">Next</button>
       </div>
     </div>
 
     <hr>
 
-    <div class="row">
-      <div class="col-md-10">
-        <div class="progress">
-          <div class="progress-bar" role="progressbar" aria-valuenow="00" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">0%</div>
+    <form>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="panel panel-default" v-show="current == i" v-for="(question, i) in questions">
+            <div class="panel-heading">
+              <h3 class="panel-title">Question #@{{ i + 1 }}</h3>
+            </div>
+            <div class="panel-body">
+              <div>@{{ question.text }}</div>
+              <div class="radio" v-for="(option, j) in question.options">
+                <label>
+                  <input type="radio" v-bind:name="'optionsRadios' + (i + 1)" v-bind:id="'optionsRadios' + (j + 1)" v-bind:value="option.id" v-on:click="answer(i)">
+                  @{{ option.text }}
+                </label>
+              </div>
+            </div>
+          </div>
+          <button class="btn btn-default" type="button" v-on:click="prev" v-bind:class="{ disabled: current == 0 }">Prev</button>
+          <button class="btn btn-primary" type="button" v-on:click="next" v-bind:class="{ disabled: current == questions.length - 1 }">Next</button>
         </div>
       </div>
-      <div class="col-md-2">
-        <button class="btn btn-primary btn-block" type="button">Submit</button>
+
+      <hr>
+
+      <div class="row" v-if="completeness == 1">
+        <div class="col-md-offset-3 col-md-6">
+          <button class="btn btn-primary btn-block" type="submit">Submit</button>
+        </div>
       </div>
-    </div>
+    </form>
 
   </div>
+@endsection
+
+@section('script')
+  <script>
+    var questions = [];
+
+    questions.push({
+      id: 1,
+      text: "What did you eat for breakfast?",
+      options: [
+        {id: 1, text: "Option 1"},
+        {id: 2, text: "Option 2"},
+        {id: 3, text: "Option 3"},
+      ],
+      answered: false
+    });
+
+    questions.push({
+      id: 2,
+      text: "Do you want to go space?",
+      options: [
+        {id: 1, text: "Option 1"},
+        {id: 2, text: "Option 2"},
+        {id: 3, text: "Option 3"},
+      ],
+      answered: false
+    });
+
+    var app = new Vue({
+      el: '#app1',
+      data: {
+        current: 0,
+        questions: questions
+      },
+      computed: {
+        completeness: function () {
+          var answered = this.questions.filter(function (question) {
+            return question.answered;
+          });
+
+          return answered.length / this.questions.length;
+        }
+      },
+      methods: {
+        next: function () {
+          if (this.current != this.questions.length - 1) {
+            this.current += 1;
+          }
+        },
+        prev: function () {
+          if (this.current != 0) {
+            this.current -= 1;
+          }
+        },
+        answer: function (index) {
+          this.questions[index].answered = true;
+        },
+        validate: function (e) {
+          if (completeness != 1) {
+            e.preventDefault();
+          }
+        }
+      }
+    });
+  </script>
 @endsection
