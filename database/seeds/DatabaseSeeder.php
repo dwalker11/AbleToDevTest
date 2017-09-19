4ea4bf8137f5ece4a666dacf01f997ec50ca5c53
Devon Walker <devon@winterfell.localdoamin>
2017-09-18 20:06:47 -0400

    Seeded the questions & options tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,61 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('questions')->truncate();
+        DB::table('options')->truncate();
+
+        $date = date("Y-m-d H:i:s");
+
+        // Question #1
+        $id = DB::table('questions')->insertGetId([
+            'text'       => "What did you eat for breakfast this morning?",
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        DB::table('options')->insert([
+            ['text' => 'Eggs & Bacon', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Oatmeal', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Nothing', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+        ]);
+
+        // Question #2
+        $id = DB::table('questions')->insertGetId([
+            'text'       => "How would you rate your current mood?",
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        DB::table('options')->insert([
+            ['text' => 'Good', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Neutral', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Bad', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+        ]);
+
+        // Question #3
+        $id = DB::table('questions')->insertGetId([
+            'text'       => "How often do you excercise?",
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        DB::table('options')->insert([
+            ['text' => 'Frequently', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Regularly', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Rarely', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+        ]);
+
+        // Question #4
+        $id = DB::table('questions')->insertGetId([
+            'text'       => "How would you rate your current stress level?",
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+        DB::table('options')->insert([
+            ['text' => 'High', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Medium', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+            ['text' => 'Low', 'question_id' => $id, 'created_at' => $date, 'updated_at' => $date],
+        ]);
     }
 }
